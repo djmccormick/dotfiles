19c0a16373e9d2ce0cfa3e5d5d5b845d430782e9
Dustin McCormick <djmccormick@users.noreply.github.com>
2024-01-21 02:05:12 -0500

    add copilot

--- nvim-custom/mappings.lua
+++ nvim-custom/mappings.lua
@@ -10,6 +10,16 @@ M.general = {
   },
 }
 
--- more keybinds!
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  }
+}
 
 return M
